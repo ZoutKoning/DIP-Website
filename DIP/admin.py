9fc629b3f7f0ae93d4b1ec6b79bbb241bd0e4d20
Joseph Suter <112408320+ZoutKoning@users.noreply.github.com>
2023-10-04 21:41:53 -0400

    Current Working DB Connection Website

---
+++ DIP/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import DIP
+# Register your models here.
+admin.site.register(DIP)
---
+++ DIP/url.py
@@ -0,0 +1,7 @@
+"""from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('about/', views.index, name='home'),
+
+]"""

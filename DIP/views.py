47d70a940de383de9bb08cbc9559eede74037dff
ZoutKoning <jfscyber9@gmail.com>
2023-10-10 16:03:30 -0400

    front end set up

--- DIP/views.py
+++ DIP/views.py
@@ -1,14 +1,6 @@
-"""from django.shortcuts import render
-from .models import Sprint
-def index (request):
-    View for about page of site
-    #Generate Sprint number
-    num_sprints = Sprint.sprint.count()
-    context = {
-        'num_sprints': num_sprints,
-    }
-    return render(request, 'index.html', context=context) """
+from django.shortcuts import render
+from django.http import HttpResponse
 
-from django.shortcuts import redirect
 def index(request):
-    return redirect('/About')
+    return render(request,'index.html')
+

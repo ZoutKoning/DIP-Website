af5dab5916f1004011b53304dba11ed6a36a1950
ZoutKoning <jfscyber9@gmail.com>
2023-10-12 16:04:34 -0400

    Commit to branch

--- WebPages/settings.py
+++ WebPages/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DIP',
+    #'DIP',
+    'DIP.apps.DipConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'WebPages.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

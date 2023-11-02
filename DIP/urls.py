26a906bb2f2146377dce0f81f113685e10a9be80
ZoutKoning <jfscyber9@gmail.com>
2023-11-02 15:57:58 -0400

    Old Code redone

--- DIP/urls.py
+++ DIP/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
+    path('wallet/', views.wallet, name ="wallet")
 ]

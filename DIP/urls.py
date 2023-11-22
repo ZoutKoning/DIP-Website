56d81b836d3f00f0182e615a201ca5ee6b84459a
ZoutKoning <jfscyber9@gmail.com>
2023-11-22 17:26:53 -0500

    form now

--- DIP/urls.py
+++ DIP/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     # path('drivers/', views.drivers, name="drivers"), #signup page
     url(r'form', views.drivers, name='form'),
     path('dashboard/', views.dashboard, name="dashboard")
+    #checking things go usless comment
 ]

e735a40cedf23466204f9143ecb9df831b4e1438
jjmoore23 <123654777+jjmoore23@users.noreply.github.com>
2023-11-27 18:19:50 -0500

    Update views.py
    Added views for the catalog and the cart

--- DIP/views.py
+++ DIP/views.py
@@ -10,25 +10,7 @@ from .forms import MyForm
 
 # Home page
 def index(request):
-    context = {}
-    try:
-        code = request.GET.get('code')
-        userData = getTokens(code)
-        context['name'] = userData['name']
-        context['status'] = 1
-        print(f'{context=}')
-        response = render(request, 'index.html', context)
-        response.set_cookie('sessiontoken', userData['id_token'], max_age=60 * 60 * 24, httponly=True)
-        return response
-    except:
-        token = getSession(request)
-        if token is not None:
-            userData = decode_jwt.lambda_handler(token, None)
-            context['name'] = userData['name']
-            context['status'] = 1
-            print(f'2){context=}')
-            return render(request, 'index.html', context)
-        return render(request, 'index.html', {'status': 0})
+    return render(request, "index.html")
 
 
 # About page
@@ -62,52 +44,10 @@ def dashboard(request):
     return render(request, "dashboard.html")
 
 
-def getTokens(code):
-    TOKEN_ENDPOINT = config('TOKEN_ENDPOINT')
-    REDIRECT_URL = config('REDIRECT_URL')
-    CLIENT_ID = config('CLIENT_ID')
-    CLIENT_SECRET = config('CLIENT_SECRET')
+# Catalog page
+def catalog(request):
+    return render(request, "catalog.html")
 
-    encodeData = base64.b64encode(bytes(f"{CLIENT_ID}:{CLIENT_SECRET}", "ISO-8859-1")).decode("ascii")
-
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': f'Basic {encodeData}'
-    }
-
-    body = {
-        'grant_type': 'authorization-code',
-        'client_id': CLIENT_ID,
-        'code': code,
-        'redirect_url': REDIRECT_URL,
-    }
-
-    response = requests.post(TOKEN_ENDPOINT, data=body, headers=headers)
-
-    id_token = response.json()['id_token']
-
-    userData = decode_jwt.lambda_handler(id_token, None)
-
-    if not userData:
-        return False
-
-    user = {
-        'id_token': id_token,
-        'name': userData['name'],
-        'email': userData['email'],
-    }
-    return user
-
-
-def getSession(request):
-    try:
-        response = request.COOKIES["sessiontoken"]
-        return response
-    except:
-        return None
-
-
-def signout(request):
-    response = render(request, 'index.html', {'status': 0})
-    response.delete_cookie('sessiontoken')
-    return response
+# Page that loads the cart
+def cart(request):
+    return render(request, "cart.html")

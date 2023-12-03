1274d6a9a64981583e6ab91c8d3ddddf75c65c78
ZoutKoning <jfscyber9@gmail.com>
2023-12-03 01:28:06 -0500

    Authenticated New User Sign Up

--- members/models.py
+++ members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Role(models.Model):
+    userRole = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.userRole

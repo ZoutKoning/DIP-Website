a7b69b3d2af06ec891917ff4bf5acc4ccca4efdc
ZoutKoning <jfscyber9@gmail.com>
2023-12-03 23:55:17 -0500

    Revert "Merge pull request #76 from ZoutKoning/Joseph_Branch"
    This reverts commit 43deff75519a5347cfd2c19c6f18a72583e6b106, reversing
    changes made to b1ae6d7f2151f3ff5e08016aabb6250de49ec234.

--- members/forms.py
+++ members/forms.py
@@ -6,11 +6,11 @@ from .models import Role
 
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    firstName = forms.CharField(max_length=100)
+    lastName = forms.CharField(max_length=100)
     Role = forms.CharField(max_length=1, help_text='D(driver) S(sponsor) A(admin)')
     # userRole = forms.ModelChoiceField(queryset=Role.objects.all())
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'Role')
+        fields = ('firstName', 'lastName', 'email', 'username', 'password1', 'password2', 'Role')

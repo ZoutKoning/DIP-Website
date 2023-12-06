529dbfa326af91f709d91984165341b2ff3b4550
Joseph Suter <112408320+ZoutKoning@users.noreply.github.com>
2023-12-06 16:56:22 -0500

    Update forms.py
    typo in last fix

--- members/forms.py
+++ members/forms.py
@@ -7,7 +7,7 @@ from .models import Role
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(max_length=100)
-    lastn_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
     Role = forms.CharField(max_length=1, help_text='D(driver) S(sponsor) A(admin)')
     # userRole = forms.ModelChoiceField(queryset=Role.objects.all())
 
